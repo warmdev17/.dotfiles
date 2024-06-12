b6f2b48b6074cee00794c706f640ee2f6984d3d0
warmdev17 <warmdevofficial@gmail.com>
2024-06-13 05:07:02 +0700

    first commit

--- install.sh
+++ install.sh
@@ -28,3 +28,24 @@ sudo pacman -Sy gum --noconfirm
 source ./.install/package.sh
 source ./.install/neovim.sh
 source ./.install/wallpaper.sh
+source ./.install/symlink/config.sh
+
+# ------------------------------------------------------
+# Reboot
+# ------------------------------------------------------
+
+echo -e "${GREEN}"
+figlet "Reboot"
+echo -e "${NONE}"
+echo "A reboot of your system is recommended."
+echo
+if gum confirm "Do you want to reboot your system now?"; then
+	gum spin --spinner dot --title "Rebooting now..." -- sleep 3
+	systemctl reboot
+elif [ $? -eq 130 ]; then
+	exit 130
+else
+	echo ":: Reboot skipped"
+fi
+echo ""
+sleep 3
